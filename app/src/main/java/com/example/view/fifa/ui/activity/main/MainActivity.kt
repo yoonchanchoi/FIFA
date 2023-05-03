f9d7f0e0da5689dcf671720bcf5d91a607ba8324
Choi YoonChan <choyc9239@gmail.com>
2023-05-03 17:15:42 +0900

    rxjava의 concatMap사용 수정 중 더 좋은 방법을 찾는중

--- app/src/main/java/com/example/view/fifa/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/example/view/fifa/ui/activity/main/MainActivity.kt
@@ -18,7 +18,14 @@ import com.example.view.fifa.R
 import com.example.view.fifa.databinding.ActivityMainBinding
 import com.example.view.fifa.ui.activity.searchsubactivity.SearchSubActivity
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.Flowable
+import io.reactivex.Single
+import io.reactivex.functions.BiFunction
+import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
+import kotlin.collections.ArrayList
+import kotlin.random.Random.Default.nextLong
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -41,7 +48,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 //        setContentView(R.layout.activity_main)
-        init()
+//        init()
+        initTest()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -137,5 +145,41 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         autoScrollStop()
     }
+    fun initTest(){
+
+        val startTime = Date().time
+        val urls = listOf(
+            "https://naver-api-1.com",
+            "https://google-api-2.com",
+            "https://samsung-api-3.com",
+            "https://kakao-api-4.com",
+            "https://line-api-5.com"
+        )
+
+        Flowable.fromIterable(urls)
+            .concatMapEager { result ->
+                request(result).toFlowable()
+            }.subscribe({
+                println("${Date().time - startTime} $it")
+            }, { error ->
+                error.printStackTrace()
+            }, {
+                println("${Date().time - startTime} complete")
+            })
+
+        Thread.sleep(3000L)
+        println("Process finished")
+    }
+
+
+    fun request(url: String): Single<String> {
+        return Single.zip(
+            Single.timer(kotlin.random.Random.nextLong(2000), TimeUnit.MILLISECONDS),
+            Single.just(url),
+            BiFunction<Long, String, String> { _, url ->
+                "$url response"
+            })
+    }
+
 
 }
